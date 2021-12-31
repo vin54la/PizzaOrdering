6cc91d6e0dfaa21623bc36a0e0d8a8ed2e0feadf
vin54la <63419265+vin54la@users.noreply.github.com>
2021-12-31 10:03:28 -0800

    Add files via upload

---
+++ pizzaordersoftware.rb
@@ -0,0 +1,18 @@
+puts "Pizza Ordering Software"
+puts "Put your order below."
+
+puts "What meat would you like?"
+meat = gets.chomp()
+puts "What crust would you like?"
+crust = gets.chomp()
+puts "What cheese would you like?"
+cheese = gets.chomp()
+puts "What vegetable would you like?"
+vegetable = gets.chomp()
+puts "Order details"
+puts ("Meat: " + meat)
+puts ("Crust: " + crust)
+puts ("Cheese: " + cheese)
+puts ("Vegetables: " + vegetable)
+puts "Please put your name."
+name = gets.chomp()
